6d83ceaa30e683e706819605c91401eeef78dee0
bekah <rebekahmercer@hotmail.co.uk>
2017-08-05 13:35:39 -0700

    making proofs!

--- prototype/grothkohlweiss.go
+++ prototype/grothkohlweiss.go
@@ -36,27 +36,72 @@ func init() {
 	H, _ = HashToCurve("i am a stupid moron".Bytes()) // check this
 }
 
-func polysetup() {
+func prove() {
 	N = len(R.PubKeys)
 	// n = log base 2 len(R.PubKeys)
 	// R hasnt even been defined yet
 	// make sure all indices are now in binary and the same length
+		var r []*big.Int
+		var a []*big.Int
+		var s []*big.Int
+		var t []*big.Int
+		var rho []*big.Int
+
+		var cl []*CurvePoint
+		var ca []*CurvePoint
+		var cb []*CurvePoint
 
 	for j := 0; j < n; j++ {
-		rj, e := rand.Int(rand.Reader, N)
+		// psa that these arrays dont actually exist?
+		// so we need to initialise them? make them slices? i dont get it?
+		// should we not replace this with something more compact (yes)
+		// do we need to append instead of filling in like this?
+		r[j], e := rand.Int(rand.Reader, N)
 		check(e)
-		aj, e := rand.Int(rand.Reader, N)
+		a[j], e := rand.Int(rand.Reader, N)
 		check(e)
-		sj, e := rand.Int(rand.Reader, N)
+		s[j], e := rand.Int(rand.Reader, N)
 		check(e)
-		tj, e := rand.Int(rand.Reader, N)
+		t[j], e := rand.Int(rand.Reader, N)
 		check(e)
-		rhok, e := rand.Int(rand.Reader, N)
+		rho[k], e := rand.Int(rand.Reader, N)
 		check(e)
+
+		// we should probs make these entries in arrays? bleh
+		cl[j] := commit(l[j], r[j])
+		ca[j] := commit(a[j], s[j])
+		cb[j] := commit(l[j] + a[j], t[j])
+
+		k := j - 1
+		var product CurvePoint
+		for i := 0; i < N; i++ {
+		// we need an array of polynomial coefficients to exist
+			temp := PubKey[j].ScalarMult(polycoeff[i][k])
+			product := product.Add(temp)
+		}
+
+		cdk := product.Add(CurvePoint{}.ScalarCaseMult(rhok))
+
+		x := sha3.Sum256("wow so much fun")
+
+		fj := lj * x + aj
+		zaj := rj * x + sj
+		zbj := rj * (x - fj) + tj
+
+		var sum *big.Int // should this be a pointer?
+		for k := 0; k < n; k++ {
+			temp := rho[k] * x**k
+			sum = sum + temp
+		}
+		zd := r * x**n - sum
+
+		// return all commitments and fj and zaj and zbj and zdk
 	}
 }
 
-func commitment(a *big.Int, b *big.Int) CurvePoint {
+
+
+func commit(a *big.Int, b *big.Int) CurvePoint {
 	ha := H.ScalarMult(a)
 	gb := CurvePoint{}.ScalarBaseMult(b)
 	return ha.Add(gb) // why don't we need to do the interface thing here? how does it know? :o

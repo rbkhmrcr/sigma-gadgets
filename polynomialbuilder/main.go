5a777cfc25e19ee01fd6d74cf0dc8e264f0d0279
bekah <rebekahmercer@hotmail.co.uk>
2017-08-10 04:14:51 -0700

    separating functions for clarity

--- polynomialbuilder/main.go
+++ polynomialbuilder/main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"crypto/rand"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
 	secp "github.com/btcsuite/btcd/btcec"
-	poly "github.com/jongukim/polynomial"
 	"golang.org/x/crypto/sha3"
 	"io/ioutil"
-	//	"math"
 	"math/big"
-	"strconv"
 )
 
 // Group is secp256k1 as defined in btcec
@@ -100,9 +96,6 @@ func main() {
 	// why not just read it from the file ?????
 	privkey.SetString("23246495091784532220524749001303194962250020895499760086019834032589186452479", 10)
 	proofa, proofb, proofc := Prover(pubkeys, 3, 2, privkey)
-	fmt.Println("proofa : ", proofa)
-	fmt.Println("proofb : ", proofb)
-	fmt.Println("proofc : ", proofc)
 
 	pv := Verify(pubkeys, 3, proofa, proofb, proofc)
 	fmt.Println("verificaaaationnnnnnn : ", pv)
@@ -147,213 +140,6 @@ func SpendVerify(pp, M, serial, C, pi) {
 
 */
 
-// Prover is the gk prover routine. it's needed in ring signatures and stuff.
-// if Ring has a length (?) then we don't need to submit the length separately :)
-func Prover(ring Ring, ringlength int, signerindex int, privatekey *big.Int) ([]CurvePoint, []*big.Int, *big.Int) {
-
-	/* -----------------------------------------
-	this is the first part of the sigma protocol
-	----------------------------------------- */
-
-	ringbin := strconv.FormatInt(int64(ringlength), 2)
-	// TODO: check if the bitlength = n is correct!!
-	n := uint(len(ringbin) + 1)
-	randomvars := make([]*big.Int, 0)
-	commitments := make([]CurvePoint, 0)
-	// j is the bitwise index, always :) in the paper it's 1, ..., n, but we'll count from 0.
-	for j := uint(0); j < n; j++ {
-		// we could use a for loop here with i from 0 to 4 ?
-		rj, e := rand.Int(rand.Reader, grouporder)
-		Check(e)
-		randomvars = append(randomvars, rj)
-		// so r[j] will be randomvars[5*j]
-		aj, e := rand.Int(rand.Reader, grouporder)
-		Check(e)
-		randomvars = append(randomvars, aj)
-		// so a[j] will be randomvars[5*j + 1]
-		sj, e := rand.Int(rand.Reader, grouporder)
-		Check(e)
-		randomvars = append(randomvars, sj)
-		// so s[j] will be randomvars[5*j + 2]
-		tj, e := rand.Int(rand.Reader, grouporder)
-		Check(e)
-		randomvars = append(randomvars, tj)
-		// so t[j] will be randomvars[5*j + 3]
-		rhok, e := rand.Int(rand.Reader, grouporder)
-		Check(e)
-		randomvars = append(randomvars, rhok)
-		// so rho[k] will be randomvars[5*j + 4]
-		// should these actually not just use the variables aj, sj, etc, as they are still
-		// set to the ones that are needed? is this lots of unnecessary array fetching?
-
-		// clj = lj * g + rj * h
-		bigintbit := big.NewInt(int64(((signerindex >> j) & 0x1)))
-		newcommitment := Commit(bigintbit, randomvars[5*j])
-		commitments = append(commitments, newcommitment)
-		// clj will be commitments[3*j]
-
-		// caj = aj * g + sj * h
-		commitments = append(commitments, Commit(randomvars[5*j+1], randomvars[5*j+2]))
-		// caj will be commitments[3*j + 1]
-
-		// cbj = (lj * aj) * g + tj * h
-		z := new(big.Int)
-		commitments = append(commitments, Commit(z.Add(bigintbit, randomvars[5*j+1]), randomvars[5*j+3]))
-
-		// cdk = (for i = 0, ..., N-1) p[i][k] * ci    +      0 * g + rhok * h
-		// product temp is p[i][k] * c[i]
-		var producttemp CurvePoint
-		for i := 0; i < ringlength; i++ {
-			// polytemp is p[i][k]
-			polytemp := PolynomialBuilder(signerindex, ringlength, i)
-			// cdk lhs is p[i][k] * c[i] for a given i
-			fmt.Println("signerindex : ", signerindex)
-			fmt.Println("i : ", i)
-			fmt.Println("j : ", j)
-			fmt.Println("poly : ", polytemp)
-			cdklhs := (ring.PubKeys[i]).ScalarMult(polytemp[j])
-
-			if i == 0 {
-				// each type we loop through the k and start on a new i we reset the product
-				producttemp = ring.PubKeys[i].ScalarMult(polytemp[j])
-			} else {
-				// we're using EC points so multiplication is really addition
-				// this is adding the latest p[i][k] * c[i] to the previous ones (for given k)
-				z := producttemp.Add(cdklhs)
-				producttemp = z
-			}
-		}
-		// cdk = above product + 0 * g + rho * h
-		newcommitment = Commit(big.NewInt(0), randomvars[5*j+4])
-		commitments = append(commitments, newcommitment.Add(producttemp))
-
-	}
-
-	/* ------------------------------------
-	this is where we generate the challenge
-	------------------------------------ */
-
-	// should we just carry on the loop above? who cares
-	// we need to convert the challenge into a big int :(
-	array := sha3.Sum256([]byte("lots of cool stuff including the commitments"))
-	challenge := Convert(array[:])
-
-	/* ------------------------------------------
-	this is the second part of the sigma protocol
-	------------------------------------------ */
-
-	var responses []*big.Int
-	for j := uint(0); j < n; j++ {
-
-		z := new(big.Int)
-		// fj = lj * x + aj
-		lj := big.NewInt(int64(((signerindex >> j) & 0x1)))
-		fj := z.Mul(lj, challenge)
-		fj = z.Mod(fj, grouporder)
-		fj = z.Add(fj, randomvars[5*j+1])
-		fj = z.Mod(fj, grouporder)
-		// so fj = responses[3*j]
-		responses = append(responses, fj)
-
-		// zaj = rj * x + sj
-		// TODO: is using z like this weird??
-		z = new(big.Int)
-		zaj := z.Mul(randomvars[5*j], challenge)
-		zaj = z.Mod(zaj, grouporder)
-		zaj = z.Add(zaj, randomvars[5*j+2])
-		zaj = z.Mod(zaj, grouporder)
-		// so zaj = responses[3*j + 1]
-		responses = append(responses, zaj)
-
-		// zbj = rj * (x - fj) + tj
-		z = new(big.Int)
-		zbj := z.Sub(challenge, fj)
-		zbj = z.Mod(zbj, grouporder)
-		zbj = z.Mul(randomvars[5*j], zbj)
-		zbj = z.Mod(zbj, grouporder)
-		zbj = z.Add(zbj, randomvars[5*j+3])
-		zbj = z.Mod(zbj, grouporder)
-		// so zbj = responses[3*j + 2]
-		responses = append(responses, zbj)
-
-	}
-
-	// zd = r * x ** n - sum from k = 0 to k = n - 1 of rhok * x ** k
-	z := new(big.Int)
-	ztemp := new(big.Int)
-	zdsum := new(big.Int)
-
-	// zd (lhs) = r * x ** n
-	rxn := z.Exp(challenge, big.NewInt(int64(n)), grouporder)
-	rxn = z.Mod(rxn, grouporder)
-	rxn = z.Mul(rxn, privatekey)
-	rxn = z.Mod(rxn, grouporder)
-
-	for k := uint(0); k < n; k++ {
-		z := new(big.Int)
-		// x ** k
-		xk := z.Exp(challenge, big.NewInt(int64(k)), grouporder)
-		// zd = SUM( rhok * x ** k )
-		zdelement := z.Mul(randomvars[5*k+4], xk)
-		zdelement = z.Mod(zdelement, grouporder)
-		// zd = sum over k of the above
-		zdsum = z.Add(zdsum, zdelement)
-		zdsum = z.Mod(zdsum, grouporder)
-		ztemp = zdsum
-	}
-
-	zd := z.Sub(rxn, ztemp)
-	zd = z.Mod(zd, grouporder)
-
-	return commitments, responses, zd
-}
-
-// Verify is the gk **proof** verification. (contrast with SpendVerify)
-func Verify(ring Ring, ringlength int, commitments []CurvePoint, responses []*big.Int, zd *big.Int) bool {
-	for i := 0; i < len(commitments); i++ {
-		check := Group.IsOnCurve(commitments[i].X, commitments[i].Y)
-		if check == false {
-			return false
-		}
-	}
-
-	array := sha3.Sum256([]byte("lots of cool stuff including the commitments"))
-	challenge := Convert(array[:])
-	fmt.Println(challenge)
-	ringbin := strconv.FormatInt(int64(ringlength), 2)
-	// TODO: check if the bitlength = n is correct!!
-	n := int(len(ringbin) + 1)
-
-	for j := 0; j < n; j++ {
-		// (x * clj) + caj == commit(fj, zaj)
-		// (challenge * commitments[4*j]) + commitments[4*j + 1] == commit(responses[3*j], responses[3*j+1])
-		xc := (commitments[4*j]).ScalarMult(challenge)
-		lhs := xc.Add(commitments[4*j+1])
-		fmt.Println("LHS : ", lhs)
-		rhs := Commit(responses[3*j], responses[3*j+1])
-		fmt.Println("RHS : ", rhs)
-		if rhs.X.Cmp(lhs.X) != 0 || rhs.Y.Cmp(lhs.Y) != 0 {
-			return false
-		}
-
-		// ((x - fj) * clj) + cbj == commit(0, zbj)
-		// ((challenge - responses[3*j]) * commitments[4*j]) + commitments[4*j+2] == commit(0, responses[3*j+2])
-		z := new(big.Int)
-		xf := z.Sub(challenge, responses[3*j])
-		xf = z.Mod(xf, grouporder)
-		xfc := (commitments[4*j]).ScalarMult(xf)
-		lhs = xfc.Add(commitments[4*j+2])
-		fmt.Println("LHS : ", lhs)
-		rhs = Commit(big.NewInt(0), responses[3*j+2])
-		fmt.Println("RHS : ", rhs)
-		if rhs.X.Cmp(lhs.X) != 0 || rhs.Y.Cmp(lhs.Y) != 0 {
-			return false
-		}
-
-	}
-	return true
-}
-
 // Commit forms & returns a pedersen commitment with the two arguments given
 func Commit(a *big.Int, b *big.Int) CurvePoint {
 	ga := CurvePoint{}.ScalarBaseMult(a)
@@ -389,79 +175,6 @@ func HashToCurve(s []byte) (CurvePoint, error) {
 		x.Add(x, big.NewInt(1))
 	}
 	return CurvePoint{}, errors.New("no curve point found")
-}
-
-// PolynomialBuilder builds the weird polynomials we need in the GK proving algo
-func PolynomialBuilder(signerindex int, ringsize int, currenti int) poly.Poly {
-
-	// this is just to print and get the bit length, n
-	// TODO: print this and see if its right
-	// signerindexbin := strconv.FormatInt(int64(signerindex), 2)
-	ringbin := strconv.FormatInt(int64(ringsize), 2)
-	// the product should be of length = bitlength(ringsize)
-	var product poly.Poly
-	// the products of functions defined by each i form distinct polynomials (one per i)
-	// this polynomial will have degree max bitlength(ringlength)
-
-	// things need to be uint so the bitshifting works
-	// len(ringbin) = n
-	// ------------------------------------------------------------------------------
-	// is it gonna cause problems that we're running 0 -> n - 1 rather than 1 -> n :(
-
-	// j is the bit index.
-	// the functions defined in this bit get multiplied together to form the poly above
-	for j := uint(0); j < uint(len(ringbin)); j++ {
-
-		var functiontemp poly.Poly
-		aj, e := rand.Int(rand.Reader, grouporder)
-		Check(e)
-		z, e := rand.Int(rand.Reader, grouporder)
-		Check(e)
-
-		// we compare i (the current index) to l (the signer index), bitwise
-		if (currenti >> j & 0x1) == 0 {
-			if ((signerindex >> j) & 0x1) == 0 {
-				// f = x - aj
-				functiontemp = append(functiontemp, z.ModInverse(aj, grouporder))
-				functiontemp = append(functiontemp, big.NewInt(1))
-			}
-			if ((signerindex >> j) & 0x1) == 1 {
-				// otherwise it's just - aj
-				functiontemp = append(functiontemp, z.ModInverse(aj, grouporder))
-				functiontemp = append(functiontemp, big.NewInt(0))
-			}
-		}
-
-		if (currenti >> j & 0x1) == 1 {
-			if ((signerindex >> j) & 0x1) == 1 {
-				// f = x + aj
-				// this mod is super redundant
-				functiontemp = append(functiontemp, z.Mod(aj, grouporder))
-				functiontemp = append(functiontemp, big.NewInt(1))
-			}
-			if ((signerindex >> j) & 0x1) == 0 {
-				// otherwise it's just aj
-				// this mod is super redundant
-				functiontemp = append(functiontemp, z.Mod(aj, grouporder))
-				functiontemp = append(functiontemp, big.NewInt(0))
-			}
-		}
-
-		if j == 0 {
-			// i should do this in some prettier way hey?
-			// is there a way to make sure the polynomials are always a certain length
-			// even is lots of entries are 0? :/
-			product = poly.NewPolyInts(0)
-			product = functiontemp
-		} else {
-			product = product.Mul(functiontemp, grouporder)
-			product = append(product, big.NewInt(0))
-			product = append(product, big.NewInt(0))
-			product = append(product, big.NewInt(0))
-			product = append(product, big.NewInt(0))
-		}
-	}
-	return product
 }
 
 // ConvertPubKeys takes the string rep of coords ('x', 'y') and changes to *big.Ints
---
+++ polynomialbuilder/verify.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/sha3"
+	"math/big"
+	"strconv"
+)
+
+// Verify is the gk **proof** verification. (contrast with SpendVerify)
+func Verify(ring Ring, ringlength int, commitments []CurvePoint, responses []*big.Int, zd *big.Int) bool {
+	for i := 0; i < len(commitments); i++ {
+		check := Group.IsOnCurve(commitments[i].X, commitments[i].Y)
+		if check == false {
+			return false
+		}
+	}
+
+	array := sha3.Sum256([]byte("lots of cool stuff including the commitments"))
+	challenge := Convert(array[:])
+	ringbin := strconv.FormatInt(int64(ringlength), 2)
+	// TODO: check if the bitlength = n is correct!!
+	n := int(len(ringbin) + 1)
+
+	for j := 0; j < n; j++ {
+		// (x * clj) + caj == commit(fj, zaj)
+		// (challenge * commitments[4*j]) + commitments[4*j + 1] == commit(responses[3*j], responses[3*j+1])
+		xc := (commitments[4*j]).ScalarMult(challenge)
+		lhs := xc.Add(commitments[4*j+1])
+		rhs := Commit(responses[3*j], responses[3*j+1])
+		if rhs.X.Cmp(lhs.X) != 0 || rhs.Y.Cmp(lhs.Y) != 0 {
+			return false
+		}
+		/*
+			// ((x - fj) * clj) + cbj == commit(0, zbj)
+			// ((challenge - responses[3*j]) * commitments[4*j]) + commitments[4*j+2] == commit(0, responses[3*j+2])
+			z := new(big.Int)
+			// challenge - responses[3*j]
+			xf := z.Sub(challenge, responses[3*j])
+			xf = z.Mod(xf, grouporder)
+			// ( challenge - responses[3*j] ) * commitments[4*j]
+			xfc := (commitments[4*j]).ScalarMult(xf)
+			// above + commitments[4*j+2]
+			lhs = xfc.Add(commitments[4*j+2])
+			fmt.Println("LHS : ", lhs)
+			// why doesn't this work :( */
+		rhs = Commit(big.NewInt(0), responses[3*j+2])
+		fmt.Println("responses[3j + 2] : ", responses[3*j+2])
+		fmt.Println("RHS : ", rhs)
+		/*
+			if rhs.X.Cmp(lhs.X) != 0 || rhs.Y.Cmp(lhs.Y) != 0 {
+				return false
+			}
+
+		*/
+
+		// product from i = 0 to i = N - 1 of ci to the power of product of (product from j = 0 to j = n - 1 f j i j)
+		// multiplied by product from k = 0 to k = n - 1 of cdk to the power of minus x**k ======== commit(0, zd)
+
+	}
+	return true
+}
